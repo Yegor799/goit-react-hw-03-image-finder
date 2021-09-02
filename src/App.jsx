1729a16336dfd629fbbf843e4640ccf443ada426
Yegor Yefimov <egoryefim@gmail.com>
2021-09-02 13:52:23 +0300

    created SearchBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import React from 'react';
+import SearchBar from './SearchBar/SearchBar';
+
+class App extends React.Component{
+  state = {
+    searchRequest: '',
+  }
+
+  handleSubmit = searchRequest => {
+      this.setState({searchRequest})
+  }
 
 
-
-function App() {
-  return (
-    
-  );
+  render(){
+    return (
+      <SearchBar onSubmit={this.handleSubmit}/>
+    )
+  }
 }
 
 export default App;
---
+++ src/SearchBar/SearchBar.jsx
@@ -0,0 +1,23 @@
+import './SearchBar.css';
+
+function SearchBar() {
+    return (
+<header className="Searchbar">
+  <form className="SearchForm">
+    <button type="submit" className="SearchForm-button">
+      <span className="SearchForm-button-label">Search</span>
+    </button>
+
+    <input
+      className="SearchForm-input"
+      type="text"
+      autocomplete="off"
+      autofocus
+      placeholder="Search images and photos"
+    />
+  </form>
+</header>
+    )
+}
+
+export default SearchBar;
