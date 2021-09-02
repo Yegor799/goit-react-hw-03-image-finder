822f55ff649a2a5a6615aeaf8471544980a3d434
Yegor <egoryefim@gmail.com>
2021-09-02 21:01:44 +0300

    add functionality for ImageGallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import SearchBar from './components/SearchBar/SearchBar';
 import ImageGallery from './components/ImageGallery/ImageGallery';
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
     searchRequest: '',
   }
 
   handleSubmit = searchRequest => {
-      this.setState({searchRequest})
+    this.setState({ searchRequest })
   }
 
 
 
 
-  render(){
+  render() {
     return (
-      <>
-      <ToastContainer autoClose={3000} />
-      <SearchBar onSubmit={this.handleSubmit} />
-      <ImageGallery searchRequest={this.state.searchRequest}/>
-      </>
+      <div className="App">
+        <ToastContainer autoClose={3000} />
+        <SearchBar onSubmit={this.handleSubmit} />
+        <ImageGallery searchRequest={this.state.searchRequest} />
+      </div>
     )
   }
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,35 +1,51 @@
 import React from 'react';
+import './ImageGallery.css'
 
 const API_KEY = '18976162-4407e31cd80a0810b100a4c9f';
 
 
-class ImageGallery extends React.Component{
+class ImageGallery extends React.Component {
 
     state = {
-        picture: null,
+        pictures: null,
         error: null,
         page: 1,
         status: 'idle'
     }
-    
+
     componentDidUpdate(prevProps, prevState) {
         const prevPicture = prevProps.searchRequest;
         const nextPicture = this.props.searchRequest;
 
         if (prevPicture !== nextPicture) {
             this.setState({ status: 'pending' })
-            
+
             fetch(`https://pixabay.com/api/?q=${nextPicture}&page=${this.state.page}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`)
-            .then(res => res.json())
-            .then(picture => console.log(picture))
+                .then(res => res.json())
+                .then(pictures => this.setState({ pictures: pictures.hits, status: 'resolved' }))
         }
     }
 
-    
+
+
 
     render() {
+        const { pictures, error, status } = this.state;
+        const { searchRequest } = this.props;
+
         return (
-<div></div>
+            <>
+                <ul className="ImageGallery">
+                    {pictures && pictures.map(({ id, webformatURL, tag }) => {
+                        return (
+                            <li key={id} className="ImageGalleryItem" >
+                                <img src={webformatURL} alt={tag} className="ImageGalleryItem-image" />
+                            </li>
+                        )
+                    })}
+                </ul>
+            </>
+
         )
     }
 }
